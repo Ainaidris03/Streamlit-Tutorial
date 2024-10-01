b0a21ed7228e61f7ad10b9138dda53f7994ab2f3
Ainaidris03 <ainaizzatihusna03@gmail.com>
2024-10-01 08:11:07 +0000

    I made changes

--- Storybook.py
+++ Storybook.py
@@ -2,7 +2,8 @@ import os
 import streamlit as st
 from openai import OpenAI
 
-my_secret = os.environ['OPENAI_API_KEY']
+#my_secret = os.environ['OPENAI_API_KEY']
+my_secret = st.secret['OPENAI_API_KEY']
 client=OpenAI(api_key=my_secret)
 
 # Story generator
